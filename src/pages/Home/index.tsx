411f206482a70e691b20286ebe6b11c3d8b8298c
Helena Paixão <hp.helenapaixao@gmail.com>
2021-07-21 22:21:27 -0300

    fix: config code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,20 +2,10 @@ import React, { useState } from 'react'
 import TagsInput from '../../components/TagsInput/TagsInput'
 
 export const Home: React.FC = () => {
-/*   function handleSelectTags(items: string[]) {
-    return items
-  } */
-/*   const [tags, setTags] = useState<string[]>([
-    'contato@rarolabs.com.br',
-    'nao-responda@rarolabs.com.br',
-  ])
- */
   return (
     <>
-      
       <h1>Hello </h1>
-    
-      </>
+    </>
   )
 }
 
