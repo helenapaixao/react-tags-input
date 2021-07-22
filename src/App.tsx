8e44af8d6b989d57461778d6462e942e487467e2
Helena Paixão <hp.helenapaixao@gmail.com>
2021-07-21 22:12:03 -0300

    fix: project config

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import React, { useState } from 'react';
-import TagsInput from './TagsInput';
+import React from 'react'
+import Home from './pages/Home'
 
 const App: React.FC = () => {
-  function handleSelecetedTags(items: string[]) {
-    return items;
-  }
-
-  const [tags, setTags] = useState<string[]>([
-    "contato@rarolabs.com.br",
-    "nao-responda@rarolabs.com.br"
-  ]);
-
-  return (
-    <div className="App">
-      <TagsInput
-        onSelectTags={handleSelecetedTags}
-        fullWidth
-        variant="outlined"
-        id="tags"
-        name="tags"
-        placeholder="add Tags"
-        label="tags"
-        tags={tags}
-      />
-    </div>
-  );
+  return <Home />
 }
 
-export default App;
+export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import TagsInput from '../../components/TagsInput/TagsInput'
+
+export const Home: React.FC = () => {
+/*   function handleSelectTags(items: string[]) {
+    return items
+  } */
+/*   const [tags, setTags] = useState<string[]>([
+    'contato@rarolabs.com.br',
+    'nao-responda@rarolabs.com.br',
+  ])
+ */
+  return (
+    <>
+      
+      <h1>Hello </h1>
+    
+      </>
+  )
+}
+
+export default Home
